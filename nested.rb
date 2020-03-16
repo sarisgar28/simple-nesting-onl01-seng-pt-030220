5ee149d529012db82b3388967f5f02849eae057e
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-16 02:54:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,26 +1,25 @@
-	require.pry
-	programmer_hash =	{
-        :grace_hopper => {
-          :known_for => "COBOL"
+	 def changing_alan
+	  programmer_hash =	{
+          :grace_hopper =>{
+          :known_for => ["COBOL"],
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
+       { alan_kay => {
+          :known_for => ["Object Orientation"],
+          :languages => ["Smalltalk", "LISP"],
+         
+       }
+        dennis_ritchie => []
           :known_for => "Unix",
-          :languages => ["C"]
-        }
+          :languages => ["C"]}
      }
-#binding.pry
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
+
+programmer_hash {[:yukihiro_matsumoto][:known_for  "Ruby"}
+programmer_hash [:yukihiro_matsumoto][:languages]=["LISP", "C"]
  programmer_hash = { :yukihiro_matsumoto => {} }
  #  :known_for => "Ruby",
  #  :languages => ["LISP", "C"]
  #}]
-programmer_hash
 end    
     
 
